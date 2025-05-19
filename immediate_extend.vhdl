c88f90cc52b8c410066658e50ff6564ab85009f9
Benjamin Schmid Ties <benni.schmidties@gmail.com>
2025-05-19 20:27:50 +0200

    fix

--- immediate_extend.vhdl
+++ immediate_extend.vhdl
@@ -23,19 +23,19 @@ begin
         sign_extended <= (others => '0');
       ----------------------------------------------------------------
       when "0010011" | "0000011" | "1100111" => -- I type instructions (ALU with register and immediate | LOAD instructions | JALR instruction)
-        sign_extended <= (others => '0') & ie_instr(31 downto 20) when ie_instr(31) = "1"
+        sign_extended <= (others => '0') & ie_instr(31 downto 20) when ie_instr(31) = '0'
       else
         (others => "1") & ie_instr(31 downto 20);
       ----------------------------------------------------------------
       when "0100011" => -- S type instructions (STORE)
-        sign_extended <= (others => '0') & ie_instr(31 downto 25) & ie_instr(11 downto 7) when ie_instr(31) = "1"
+        sign_extended <= (others => '0') & ie_instr(31 downto 25) & ie_instr(11 downto 7) when ie_instr(31) = '0'
       else
         (others => "1") & ie_instr(31 downto 25) & ie_instr(11 downto 7);
       ----------------------------------------------------------------
       when "1100011" => -- B type instructions (BRANCH)
         sign_extended <= (others => '0') & (ie_instr(31) & ie_instr(7) & ie_instr(30 downto 25) & ie_instr(11 downto 8)) when ie_instr(31) = '0'
       else
-        (others => '0') & (ie_instr(31) & ie_instr(7) & ie_instr(30 downto 25) & ie_instr(11 downto 8));
+        (others => '1') & (ie_instr(31) & ie_instr(7) & ie_instr(30 downto 25) & ie_instr(11 downto 8));
       ----------------------------------------------------------------
       when "1101111" => -- J type instructions (only JAL)
         sign_extended <= (others => '0') & (ie_instr(31) & ie_instr(19 downto 12) & ie_instr(20) & ie_instr(30 downto 21)) when ie_instr(31) = '0'
